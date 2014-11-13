c27cac28534c67c5b839c20ce699da9b5040824e
aquincum <aquincum@users.noreply.github.com>
2014-11-13 01:01:24 -0500

    Turning FreqDists to Monoids

--- Hanalyze/FreqDist.hs
+++ Hanalyze/FreqDist.hs
@@ -17,11 +17,16 @@ type Token = String
 -- |Words are segmented to phonemes, where diphthongs, long vowels and geminates are treated as one Segment.
 type Segment = String
 -- |The frequency distribution: it is a map where keys are types and the values show the token frequency.
-type FreqDist = Map.Map Token Integer
+newtype FreqDist = FreqDist {getMap :: (Map.Map Token Integer)} deriving (Eq,Show)
+
+instance Monoid FreqDist where
+  mempty = fdEmpty
+  -- |Appending two 'FreqDist's by adding up the values in keys
+  mappend = \left right -> FreqDist $ Map.unionWith (+) (getMap left) (getMap right)
 
 -- |The empty FreqDist map.
 fdEmpty :: FreqDist
-fdEmpty = Map.empty
+fdEmpty = FreqDist Map.empty
 
 -- |Loads a corpus file into a list of tokens.
 loadFile :: FilePath -> IO [Token]
@@ -34,7 +39,7 @@ loadFile fn = do
 -- |Updates a 'FreqDist' with a token: adds +1 if the token exists as a key
 -- or inserts a key with a value of 1 if this is not the case.o
 addPlusToken :: FreqDist -> Token -> FreqDist
-addPlusToken fd tok = Map.insertWith (+) tok 1 fd
+addPlusToken fd tok = FreqDist $ Map.insertWith (+) tok 1 (getMap fd)
 
 -- |Creates a 'FreqDist' out of a set of tokens.
 countFreqs :: [Token] -> FreqDist
@@ -46,15 +51,12 @@ readCountFreqs fn = fmap countFreqs (loadFile fn)
 
 -- |Reads frequency distributions from a list of files. First, it calls 'readCountFreq' on files
 -- and then merges these distributions by adding the frequencies together.
---
--- First, we read in the files to separate FreqDists. This is (fmap readCountFreqs fns),
--- which is a ['FreqDist']. Now we make a fold over this. The initial accumulator is pure
--- Map.empty :: IO FreqDist, and the folding function merges the accumulator with the 
--- new FreqDist with unionWith: if a key is found in both maps, they are summed over.
--- Inside the lambda: Map.unionWith (+) takes 2 maps, therefore it is used in an applicative
--- manner, as x and y are IO monads. Or something like this.
 multiReadCountFreqs :: [FilePath] -> IO FreqDist
-multiReadCountFreqs fns = foldl (\x y -> Map.unionWith (+) <$> x <*> y) (pure Map.empty) (fmap readCountFreqs fns)
+multiReadCountFreqs fns = do
+  fds <- sequence $ fmap readCountFreqs fns -- [FreqDist] <- IO [FreqDist]
+  return $ mconcat fds
+                              
+
 
 -- |Recursively write out the token frequencies in a 'FreqDist', ordered in theory, but needs fixing.
 -- Initiate with a Nothing Handle
@@ -66,9 +68,9 @@ writeCountFreqs fd fn Nothing = do
   writeCountFreqs fd fn $ Just handle
   hClose handle
   return ()
-writeCountFreqs fd fn (Just handle) = let ((mkey,mval),fd2) = Map.deleteFindMax fd in do
+writeCountFreqs fd fn (Just handle) = let ((mkey,mval),mfd2) = Map.deleteFindMax (getMap fd) in do
     hPutStrLn handle (mkey ++ "\t" ++ show mval)
-    writeCountFreqs fd2 fn $ Just handle
+    writeCountFreqs (FreqDist mfd2) fn $ Just handle
 
 -- |Saves a 'FreqDist' to a file, using 'writeCountFreqs' inside.
 saveCountFreqs :: FreqDist -> FilePath -> IO ()

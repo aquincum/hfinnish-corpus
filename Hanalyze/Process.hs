addffc80dc9473ab19e1141310d6cffe94f4a7a9
aquincum <aquincum@users.noreply.github.com>
2014-11-19 18:33:03 -0500

    DOESN'T RUN! Thread blocked indefinitely.. error. Baseline

--- Hanalyze/Process.hs
+++ Hanalyze/Process.hs
@@ -1,6 +1,6 @@
 module Hanalyze.Process (
   Result, Process(..),
-  startProcess, endProcess,
+  startProcess, endProcess, waitProcess,
   initProcess, copyProcess
   ) where
 
@@ -23,9 +23,14 @@ data Process = Process {getCounter :: Counter, getBlocker :: Blocker, getResult 
 maxProcesses :: Int
 maxProcesses = 5
 
+debug :: String -> IO ()
+debug = putStrLn
+
+
 -- |Initializes a new process with new MVars all around
 initProcess :: IO Process
 initProcess = do
+  debug "initProcess"
   cnt <- newMVar 0
   blk <- newMVar ()
   res <- newMVar 0
@@ -34,48 +39,55 @@ initProcess = do
 -- |Copies the counter and blocker MVars to a new process, but initializes
 -- a new result
 copyProcess :: Process -> IO Process
-copyProcess pr = do
+copyProcess (Process c b r) = do
+  debug "copyProcess"
   newres <- newMVar 0
-  let pr2 = Process (getCounter pr) (getBlocker pr) newres
+  let pr2 = Process c b newres
   return pr2
 
 -- |Sets the result within a process
 setResult :: Int -> Process -> IO Process
 setResult i pr = do
+  debug "setResult"
   let res = getResult pr
   putMVar res i
   return pr
 
 -- |Increments the counter (adding a new process)
 incCounter :: Process -> IO Process
-incCounter pr = do
-  cntval <- takeMVar $ getCounter pr
-  putMVar (getCounter pr) (cntval + 1)
+incCounter pr@(Process c _ _) = do
+  debug "incCounter"
+  cntval <- takeMVar c
+  putMVar c (cntval + 1)
   return pr
 
 -- |Deals with determining blockage, with incrementing the counter too
 loadProcess :: Process -> IO Process
-loadProcess pr = do
-  blocker <- takeMVar $ getBlocker pr -- WILL BE BLOCKED UNTIL GO
-  cntval <- takeMVar $ getCounter pr
+loadProcess pr@(Process c b r) = do
+  debug "loadProcess"
+  blocker <- takeMVar b -- WILL BE BLOCKED UNTIL GO
+  cntval <- takeMVar c
+  _ <- takeMVar r -- let's take out result so that we can wait for that
   let newcnt = cntval + 1
-  putMVar (getCounter pr) newcnt
-  unless (newcnt >= maxProcesses) (putMVar (getBlocker pr) blocker) -- put blocker back if we're still good
+  putMVar c newcnt
+  unless (newcnt >= maxProcesses) (putMVar b blocker) -- put blocker back if we're still good
   return pr
 
 -- |Clears up counter, lets blockage go
 unloadProcess :: Process -> IO Process
-unloadProcess pr = do
+unloadProcess pr@(Process c b r) = do
   -- we have to put blocker if things good
-  let _ = tryPutMVar (getBlocker pr) () -- put blocker back if it is not there.
-  cntval <- takeMVar $ getCounter pr
-  putMVar (getCounter pr) (cntval - 1)
+  debug "unloadProcess"
+  let _ = tryPutMVar b () -- put blocker back if it is not there.
+  cntval <- takeMVar c
+  putMVar c (cntval - 1)
   return pr
 
 
 -- |Starts a process to run and registers that in the process system
 startProcess :: IO  () -> Process -> IO Process
 startProcess io pr = do
+  debug "startProcess"
   loadProcess pr -- WILL BE BLOCKED UNTIL GO
   forkFinally io (\_ -> do
                         let _ = endProcess pr
@@ -83,7 +95,15 @@ startProcess io pr = do
                  )
   return pr
 
+-- |Waits until result is given
+waitProcess :: Process -> IO Process
+waitProcess pr@(Process _ _ r) = do
+  debug "waitProcess"
+  res <- takeMVar r
+  return pr
+
 -- |Finishes running a process and deals with all the side effects
 endProcess :: Process -> IO Process
 endProcess pr = do
+  debug "endProcess"
   setResult 1 pr >>= unloadProcess
--- main.hs
+++ main.hs
@@ -9,6 +9,7 @@ import Control.Concurrent
 import Control.DeepSeq
 import Hanalyze.FreqDist
 import Hanalyze.Vowels
+import Hanalyze.Process
 
 qsort (p:xs) = qsort [x | x<-xs, x<p] ++ [p] ++ qsort [x | x<-xs, x>=p]
 
@@ -31,22 +32,10 @@ sequentialMain fns = do
   let filtered = FreqDist $ Map.filterWithKey (\s _ -> relevantStem (segment s) []) (getMap fd)
   saveFreqDist filtered "out.txt"
 
-myForkIO :: IO () -> IO (MVar Int)
-myForkIO io = do
-  mvar <- newMVar 0 
-  forkFinally io (\_ -> putMVar mvar 1)
-  return mvar
-
 main :: IO ()
 main = do
   setNumCapabilities 3
   fns <- getArgs
   ncap <- getNumCapabilities
+  initProcess >>= startProcess (dealWithAFile $ head fns) >>= waitProcess
   putStrLn $ "Running with " ++ (show ncap) ++ " capabilities."
-  let doAFile = (\file -> myForkIO (dealWithAFile file))
-  mvars <- sequence $ map doAFile fns
-  let sx = sequence $ map takeMVar mvars
-  finished <- M.liftM (all (\x -> x==1)) sx
-  sxnf <- sx
-  let _ = force sxnf
-  putStrLn $ "Wrapped up " ++ (if finished then "with all." else "with some.")
